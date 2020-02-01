4dcd09a122f3345a3443aa2bba16e56e69e7eb64
Manuel Montoya <manuwhs@gmail.com>
2020-02-01 19:31:20 +0100

    first working version

---
+++ apps/dash-oil-and-gas/mqq_utils.py
@@ -0,0 +1,76 @@
+import paho.mqtt.client as mqtt
+import queue
+import datetime as dt
+import pandas as pd 
+
+import pathlib
+
+class MQTTCONFIG:
+    HOST = "mqtt.eclipse.org"
+    PORT = 1883
+    KEEPALIVE = 60
+    SUBSCRIPTIONS = ["house/light", "house/toast"]
+
+def get_configured_mqtt_client(list_messages ):
+    client = mqtt.Client()
+    client.on_connect = on_connect_closure()
+    client.on_message = on_message_closure(list_messages)
+
+    client.connect(MQTTCONFIG.HOST, MQTTCONFIG.PORT, MQTTCONFIG.KEEPALIVE)
+    client.loop_start() 
+    
+    for subs in MQTTCONFIG.SUBSCRIPTIONS:
+        print("Subscribed to: " + subs)
+        client.subscribe(subs)
+    return client 
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect_closure():
+    def on_connect(client, userdata, flags, rc):
+        print("Connected with result code "+str(rc))
+
+        # Subscribing in on_connect() means that if we lose the connection and
+        # reconnect then subscriptions will be renewed.
+        # client.subscribe("$SYS/#")
+    return on_connect
+
+# The callback for when a PUBlLISH message is received from the server.
+def on_message_closure(list_messages: queue.Queue):
+    def on_message(client, userdata, msg):
+        df = convert_message_to_pandas(msg)
+        save_message_to_csv(df)
+        list_messages.put(df)
+    return on_message
+
+def convert_message_to_pandas(msg):
+    now = dt.datetime.now()
+    data = {"Registration Timestamp":[now],"Topic":[msg.topic], "Value": [msg.payload.decode('UTF-8')]}
+    df = pd.DataFrame.from_dict(data).set_index("Registration Timestamp")
+    return df
+
+def get_message_path(topic):
+    path_list = str(pathlib.Path(__file__)).split("/")
+    path_list.pop(-1)
+    path = "/".join(path_list) + "/data/"
+
+    path = path + topic.replace("/","_") + ".csv"
+    return path
+
+def load_topic_df(topic):
+    csv_file = get_message_path(topic)
+    try:
+        print("Loading topic: ", topic, csv_file)
+        all_df = pd.read_csv(csv_file).set_index("Registration Timestamp")
+    except:
+        # There was no file, first entry.
+        return pd.DataFrame(columns= ["Registration Timestamp", "Topic", "Value"]).set_index("Registration Timestamp")
+
+    return all_df
+
+def save_message_to_csv(df: pd.DataFrame):
+    print (df)
+    topic = df["Topic"][0]
+    all_df = load_topic_df(topic)
+    all_df = pd.concat([all_df, df])
+    csv_file = get_message_path(topic)
+    all_df.to_csv(csv_file)
---
+++ apps/dash-oil-and-gas/utils.py
@@ -0,0 +1,127 @@
+import pickle
+import copy
+import pathlib
+import dash
+import math
+import datetime as dt
+import pandas as pd
+
+# Multi-dropdown options
+from controls import COUNTIES, WELL_STATUSES, WELL_TYPES, WELL_COLORS
+
+# Create controls
+county_options = [
+    {"label": str(COUNTIES[county]), "value": str(county)} for county in COUNTIES
+]
+
+well_status_options = [
+    {"label": str(WELL_STATUSES[well_status]), "value": str(well_status)}
+    for well_status in WELL_STATUSES
+]
+
+well_type_options = [
+    {"label": str(WELL_TYPES[well_type]), "value": str(well_type)}
+    for well_type in WELL_TYPES
+]
+
+# Create global chart template
+mapbox_access_token = "pk.eyJ1IjoiamFja2x1byIsImEiOiJjajNlcnh3MzEwMHZtMzNueGw3NWw5ZXF5In0.fk8k06T96Ml9CLGgKmk81w"
+
+layout = dict(
+    autosize=True,
+    automargin=True,
+    margin=dict(l=30, r=30, b=20, t=40),
+    hovermode="closest",
+    plot_bgcolor="#F9F9F9",
+    paper_bgcolor="#F9F9F9",
+    legend=dict(font=dict(size=10), orientation="h"),
+    title="Satellite Overview",
+    mapbox=dict(
+        accesstoken=mapbox_access_token,
+        style="light",
+        center=dict(lon=-78.05, lat=42.54),
+        zoom=7,
+    ),
+)
+
+
+
+# Helper functions
+def human_format(num):
+    if num == 0:
+        return "0"
+
+    magnitude = int(math.log(num, 1000))
+    mantissa = str(int(num / (1000 ** magnitude)))
+    return mantissa + ["", "K", "M", "G", "T", "P"][magnitude]
+
+
+def filter_dataframe(df, points, well_statuses, well_types, year_slider):
+    dff = df[
+        df["Well_Status"].isin(well_statuses)
+        & df["Well_Type"].isin(well_types)
+        & (df["Date_Well_Completed"] > dt.datetime(year_slider[0], 1, 1))
+        & (df["Date_Well_Completed"] < dt.datetime(year_slider[1], 1, 1))
+    ]
+    return dff
+
+
+def produce_individual(api_well_num, points):
+    try:
+        points[api_well_num]
+    except:
+        return None, None, None, None
+
+    index = list(
+        range(min(points[api_well_num].keys()), max(points[api_well_num].keys()) + 1)
+    )
+    gas = []
+    oil = []
+    water = []
+
+    for year in index:
+        try:
+            gas.append(points[api_well_num][year]["Gas Produced, MCF"])
+        except:
+            gas.append(0)
+        try:
+            oil.append(points[api_well_num][year]["Oil Produced, bbl"])
+        except:
+            oil.append(0)
+        try:
+            water.append(points[api_well_num][year]["Water Produced, bbl"])
+        except:
+            water.append(0)
+
+    return index, gas, oil, water
+
+
+def produce_aggregate(selected, year_slider):
+
+    index = list(range(max(year_slider[0], 1985), 2016))
+    gas = []
+    oil = []
+    water = []
+
+    for year in index:
+        count_gas = 0
+        count_oil = 0
+        count_water = 0
+        for api_well_num in selected:
+            try:
+                count_gas += points[api_well_num][year]["Gas Produced, MCF"]
+            except:
+                pass
+            try:
+                count_oil += points[api_well_num][year]["Oil Produced, bbl"]
+            except:
+                pass
+            try:
+                count_water += points[api_well_num][year]["Water Produced, bbl"]
+            except:
+                pass
+        gas.append(count_gas)
+        oil.append(count_oil)
+        water.append(count_water)
+
+    return index, gas, oil, water
